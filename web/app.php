8ac689e73cfb3e530e7565878a4b7e47d4a19b1b
mrAndersen <mrandersen2004@yandex.ru>
2015-12-26 22:46:33 +0300

    fisrt page

--- web/app.php
+++ web/app.php
@@ -2,5 +2,15 @@
 
 require_once('../vendor/autoload.php');
 
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
+
+
+$loader = new Twig_Loader_Filesystem('../src/views');
+$twig = new Twig_Environment($loader);
+$view =  $twig->render('index.html.twig');
+
+
+$response = new \Symfony\Component\HttpFoundation\Response($view);
+$response->send();
 
