2a2fc1c3eb1da9903edc10e360608d7ed15e53c3
mrAndersen <mrandersen2004@yandex.ru>
2015-12-28 00:01:37 +0300

    preloader included

--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -10,7 +10,7 @@ class Helper {
         $photo      = base64_decode(explode(',',$rawBase64)[1]);
         $photo      = imagecreatefromstring($photo);
 
-        $name       = time().mt_rand(0,999).'.png';
+        $name       = time().mt_rand(0,999999999).'.png';
         $photo      = imagepng($photo,$name);
 
         return $name;
